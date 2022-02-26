0c0d10c1949a69e8af8e99b52a8c4673da604688
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-26 17:29:29 +0900

    バリデーション

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -1,2 +1,13 @@
 class Calc < ApplicationRecord
+
+  validates :ten_thousand, format: { with: /^[0-9]+$/ }
+  validates :five_thousand, format: { with: /^[0-9]+$/ }
+  validates :one_thousand, format: { with: /^[0-9]+$/ }
+  validates :five_hundred, format: { with: /^[0-9]+$/ }
+  validates :one_hundred, format: { with: /^[0-9]+$/ }
+  validates :fifty, format: { with: /^[0-9]+$/ }
+  validates :ten, format: { with: /^[0-9]+$/ }
+  validates :five, format: { with: /^[0-9]+$/ }
+  validates :one, format: { with: /^[0-9]+$/ }
+
 end
