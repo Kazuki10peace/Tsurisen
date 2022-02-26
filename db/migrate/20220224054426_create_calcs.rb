6df8bca75af323bd184daeebc2f3c452ce57a1fe
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-02-26 16:56:46 +0900

    コントローラー等

--- db/migrate/20220224054426_create_calcs.rb
+++ db/migrate/20220224054426_create_calcs.rb
@@ -6,7 +6,7 @@ class CreateCalcs < ActiveRecord::Migration[6.0]
       t.integer :one_thousand  , null: false
       t.integer :five_hundred  , null: false
       t.integer :one_hundred   , null: false
-      t.integer :fiffty        , null: false
+      t.integer :fifty         , null: false
       t.integer :ten           , null: false
       t.integer :five          , null: false
       t.integer :one           , null: false
