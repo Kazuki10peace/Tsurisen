944f45a3bd14265718e001e69822040af3d8fb6a
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-03-08 13:18:01 +0900

    新規登録実装

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -1,13 +1,13 @@
 class Calc < ApplicationRecord
 
-  validates :ten_thousand, format: { with: /^[0-9]+$/ }
-  validates :five_thousand, format: { with: /^[0-9]+$/ }
-  validates :one_thousand, format: { with: /^[0-9]+$/ }
-  validates :five_hundred, format: { with: /^[0-9]+$/ }
-  validates :one_hundred, format: { with: /^[0-9]+$/ }
-  validates :fifty, format: { with: /^[0-9]+$/ }
-  validates :ten, format: { with: /^[0-9]+$/ }
-  validates :five, format: { with: /^[0-9]+$/ }
-  validates :one, format: { with: /^[0-9]+$/ }
+  validates :ten_thousand, format: { with: /\A^[0-9]+$\z/ }
+  validates :five_thousand, format: { with: /\A^[0-9]+$\z/ }
+  validates :one_thousand, format: { with: /\A^[0-9]+$\z/ }
+  validates :five_hundred, format: { with: /\A^[0-9]+$\z/ }
+  validates :one_hundred, format: { with: /\A^[0-9]+$\z/ }
+  validates :fifty, format: { with: /\A^[0-9]+$\z/ }
+  validates :ten, format: { with: /\A^[0-9]+$\z/ }
+  validates :five, format: { with: /\A^[0-9]+$\z/ }
+  validates :one, format: { with: /\A^[0-9]+$\z/ }
 
 end
