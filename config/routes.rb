fe3483b961eda57915e3d8712aaa412b0c503ecb
Kazuki10peace <kazuki10asobo@ezweb.ne.jp>
2022-03-08 19:44:44 +0900

    基礎記述終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'calcs#index'
   resources :calcs, only: :index
+  resources :users, only: [:show, :update]
 end
